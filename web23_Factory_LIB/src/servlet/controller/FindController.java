d9ea64e93bef5caf88d823372a320ce5c91403dc
wpdud94 <62693951+wpdud94@users.noreply.github.com>
2020-06-12 16:46:28 +0900

    Add files via upload

---
+++ web23_Factory_LIB/src/servlet/controller/FindController.java
@@ -0,0 +1,24 @@
+package servlet.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import servlet.model.MemberDAOImpl;
+import servlet.model.MemberVO;
+
+public class FindController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String id=request.getParameter("id");
+		MemberVO vo=MemberDAOImpl.getInstance().findByIdMember(id);
+		ModelAndView mv=new ModelAndView();
+		mv.setViewName("find_ok");
+		mv.addObject("message"+"Hello~! Spring Framwork "+vo.getName());
+		return mv;
+	}
+
+}
